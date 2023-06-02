3d1bfe1ef517e007d8710ffb8e8e2c8fe4eff66b
João Vitor Vieira <84626557+joaovflorisvaldo@users.noreply.github.com>
2023-06-02 17:34:23 -0300

    Add files via upload

---
+++ src/main/java/com/masterclass/desafio/bandas.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.masterclass.desafio;
+
+/**
+ *
+ * @author joaovitor
+ */
+public class bandas {
+    private String nome;
+    private int posicao;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getPosicao() {
+        return posicao;
+    }
+
+    public void setPosicao(int posicao) {
+        this.posicao = posicao;
+    }
+
+
+}
